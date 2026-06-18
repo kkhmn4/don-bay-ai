506d3bb703bf2ed31077ba8735c60b7d2f0cadcf
Z User <z@container>
2026-06-18 13:35:54 +0000

    chore: clean up sandbox temp scripts from git tracking

--- .zscripts/run-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting next dev..."
-  ./node_modules/.bin/next dev -p 3000 2>&1 | tee -a /home/z/my-project/dev.log
-  echo "[$(date)] next dev exited with code $?, restarting in 3s..."
-  sleep 3
-done
